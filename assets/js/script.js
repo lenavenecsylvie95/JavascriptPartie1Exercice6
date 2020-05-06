d413ebf61ded815950c0bf21034a22f8c8b4cd70
Sylvie et Lhétie <sylvie@MacBook-Pro-de-Sylvie.local>
2020-05-07 00:20:42 +0200

    Exercice 6

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+function validate(){
+  var size = document.getElementById('shoeSize').value;
+  var birth = document.getElementById('birth').value;
+  var multi = ((size * 2 + 5) * 50 - birth) + 1769;
+  alert(multi);
+}
